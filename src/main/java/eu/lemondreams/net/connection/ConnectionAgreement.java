4162da815c18ddcb380b0e7ea7fb2b18dfeebacb
Fionan <fionanobrien@gmail.com>
2024-03-05 13:47:43 +0000

    Improvements made, Still Need to build out unit testing to determine if stable.
    Added more idempotent socket.close() operations, we can use these on all errors ensuring we kill/close sockets no longer needed.
    
    Readme added

--- src/main/java/eu/lemondreams/net/connection/ConnectionAgreement.java
+++ src/main/java/eu/lemondreams/net/connection/ConnectionAgreement.java
@@ -71,48 +71,6 @@ public class ConnectionAgreement {
 
     }
 
-    @Deprecated
-    private ArrayList<IPAddressWithSubnet> ipMatchCheck_old(ArrayList<IPAddressWithSubnet> foreignIpList) {
-
-        ArrayList<String> validIp = new ArrayList<>();
-
-
-        for (int i = 0; i < localIpList.size(); i++) {
-
-            for (int j = 0; j < foreignIpList.size(); j++) {
-
-                String tmpLocal = localIpList.get(i).getIpAddress();
-                String tmpForeign = foreignIpList.get(j).getIpAddress();
-
-                System.out.println(tmpLocal + " : " + tmpForeign);
-
-                String localIP = extractIPAddressFromCIDR(tmpLocal);
-                String foreignIP = extractIPAddressFromCIDR(tmpForeign);
-
-                System.out.println(localIP + " : " + foreignIP);
-
-
-                String localSubnet = extractSubnetMaskFromCIDR(String.valueOf(localIpList.get(i)));
-                String foreignSubnet = extractSubnetMaskFromCIDR(String.valueOf(foreignIpList.get(j)));
-
-
-                if (foreignSubnet.equalsIgnoreCase(localSubnet)) {
-
-                    if (areInSameNetwork(localIP, foreignIP, localSubnet)) {
-
-                        validIp.add(tmpLocal);
-
-                    }
-
-                }
-            }
-
-        }
-
-        return null;
-
-    }
-
     public ConnectionOffer getLOCAL() {
         return LOCAL;
     }
